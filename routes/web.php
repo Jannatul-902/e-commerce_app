8667a0b67ac80edc95a86de0e0972119e1dc3065
Jannatul-902 <ferdous15-3902@diu.edu.bd>
2025-04-06 17:09:21 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+route::get('/', [HomeController::class, 'index']);
+
+route::get('/redirect', [HomeController::class, 'redirect']);
